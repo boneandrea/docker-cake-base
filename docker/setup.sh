bf43e30c4ee7af9cbc084b8372614d18b45a2f5d
boneandrea <bone.andrea.anthrax@gmail.com>
2022-09-15 09:47:17 +0900

    .

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-cd docker
+#docker-compose build # --no-cache
+#docker-compose up # -d
 
-docker-compose build --no-cache
-docker-compose up # -d
-
-docker-compose run --rm app bin/cake version
+docker-compose run --rm web bin/cake version
 
 # DB migrate
-docker-compose run --rm app bin/cake migrations status
-docker-compose run --rm app bin/cake migrations migrate
+docker-compose run --rm web bin/cake migrations status
+docker-compose run --rm web bin/cake migrations migrate
 
 # Sessiontable
 #docker exec -i dog_mysql bash -c "mysql stg_pet_lifestyle_com -u root -proot" < ../config/schema/sessions.sql
 
 # Seeding
-#docker exec -it dog_app bash -c "bin/cake migrations seed"
+#docker exec -it dog_web bash -c "bin/cake migrations seed"
